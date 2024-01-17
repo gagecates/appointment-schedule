ed22e3d8199ab5d158bed3dd4567d778d16f05dc
Gage Cates <gagecates@GagesBlkBookPro.lan>
2024-01-17 09:59:41 -0600

    Fixes

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,6 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-
 // auth trigger for new user signup
 exports.newUserSignup = functions.auth.user().onCreate((user) => {
   console.log("user created", user.email, user.uid);
@@ -26,71 +25,73 @@ exports.getUserData = functions.https.onCall((data, context) => {
   const id = context.auth.uid;
   const promise = admin.firestore().collection("users").doc(id).get();
   return promise
-      .then((doc) => {
-        if (!doc.exists) {
-          console.log("No such document!");
-          return {response: "Not Found"};
-        }
-        console.log(doc.data());
-        console.log("worked");
-        return doc.data();
-      })
-      .catch((err) => {
-        console.log("Error getting document", err);
-      });
+    .then((doc) => {
+      if (!doc.exists) {
+        console.log("No such document!");
+        return { response: "Not Found" };
+      }
+      console.log(doc.data());
+      console.log("worked");
+      return doc.data();
+    })
+    .catch((err) => {
+      console.log("Error getting document", err);
+    });
 });
 
 // get all users
 exports.getUsers = functions.https.onCall((data, context) => {
   const promise = admin.firestore().collection("users").get();
-  return promise
-      .then((doc) => {
-        const users = [];
-        doc.forEach((doc) => {
-          users.push(doc.data());
-        });
-        return users;
-      });
+  return promise.then((doc) => {
+    const users = [];
+    doc.forEach((doc) => {
+      users.push(doc.data());
+    });
+    return users;
+  });
 });
 
 // add a new appointment request
 exports.addNewAppointment = functions.https.onCall((data, context) => {
   const userRef = admin.firestore().collection("users").doc(data.id);
-  return userRef.update({
-    newApt: admin.firestore.FieldValue.arrayUnion(data.apt),
-  })
-      .then(() => {
-        console.log("Document successfully updated!");
-      })
-      .catch((error) => {
-        console.error("Error updating document: ", error);
-      });
+  return userRef
+    .update({
+      newApt: admin.firestore.FieldValue.arrayUnion(data.apt),
+    })
+    .then(() => {
+      console.log("Document successfully updated!");
+    })
+    .catch((error) => {
+      console.error("Error updating document: ", error);
+    });
 });
 
 // update users appointments
 exports.updateAppointments = functions.https.onCall((data, context) => {
   const userRef = admin.firestore().collection("users").doc(data.id);
-  return userRef.update({
-    appointments: admin.firestore.FieldValue.arrayUnion(data.apt),
-  })
-      .then(() => {
-        console.log("Appointments successfully updated!");
-      })
-      .catch((error) => {
-        console.error("Error updating document: ", error);
-      });
+  return userRef
+    .update({
+      appointments: admin.firestore.FieldValue.arrayUnion(data.apt),
+    })
+    .then(() => {
+      console.log("Appointments successfully updated!");
+    })
+    .catch((error) => {
+      console.error("Error updating document: ", error);
+    });
 });
 
 // update logged in users appointment requests
 exports.updateNewAppointments = functions.https.onCall((data, context) => {
   const userRef = admin.firestore().collection("users").doc(data.id);
-  return userRef.update({
-    newApt: data.newApt,
-  })
-      .then(() => {
-        console.log("Appointments successfully updated!");
-      })
-      .catch((error) => {
-        console.error("Error updating document: ", error);
-      });
+  return userRef
+    .update({
+      newApt: data.newApt,
+    })
+    .then(() => {
+      console.log("Appointments successfully updated!");
+    })
+    .catch((error) => {
+      console.error("Error updating document: ", error);
+    });
 });
